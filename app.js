f178688c9f5d0814c632caf1e5238eb5b5ea1df0
JuliGarc91 <julissagarcia@pursuit.org>
2025-03-17 14:08:16 -0400

    generated firebase base64 key

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const app = express()
 // MIDDLEWARE
 app.use(cors())
 
-const multer = require('multer'); // used for file uploads
-const upload = multer(); // In-memory storage for file uploads
+// const multer = require('multer'); // used for file uploads
+// const upload = multer(); // In-memory storage for file uploads
 
 app.use((req, _res, next) => {
   console.log('Origin Requested:', req.headers.origin)
@@ -33,17 +33,17 @@ app.get('*', (_req, res) => {
 })
 
 // Route to upload an image to Cloudinary
-app.post('/upload', upload.single('file'), async (req, res) => {
-  try {
-    const result = await cloudinary.uploader.upload(req.file.buffer, {
-      folder: 'memorial-images', // specify the folder in Cloudinary
-    });
-    res.json(result); // Send back the URL or result of the upload
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error uploading image');
-  }
-});
+// app.post('/upload', upload.single('file'), async (req, res) => {
+//   try {
+//     const result = await cloudinary.uploader.upload(req.file.buffer, {
+//       folder: 'memorial-images', // specify the folder in Cloudinary
+//     });
+//     res.json(result); // Send back the URL or result of the upload
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send('Error uploading image');
+//   }
+// });
 
 // EXPORT
 module.exports = app
--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const app = require("./app.js");
 require("dotenv").config();
 const PORT = process.env.PORT || 3003;
 
-const cloudinary = require('cloudinary').v2;
+// const cloudinary = require('cloudinary').v2;
 
-// Configure Cloudinary with environment variables
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+// // Configure Cloudinary with environment variables
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
 
 
 // LISTEN
